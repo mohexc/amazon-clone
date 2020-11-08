c620fa6a72d44e7dc885da5b8e06d4c477b33c3e
nanut <galunyu@gmail.com>
2020-11-08 09:36:29 +0700

    [Added] firebase config

---
+++ src/configs/firebase.js
@@ -0,0 +1,31 @@
+import * as firebase from 'firebase'
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASEURL,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID,
+
+})
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth()
+const storage = firebase.storage()
+const provider = new firebase.auth.GoogleAuthProvider()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { db, auth, storage, timestamp, provider };
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDT9Ttz-4SEOX3eCv4BdHZAIu4y0t6FUso",
+    authDomain: "clone-7ee5c.firebaseapp.com",
+    databaseURL: "https://clone-7ee5c.firebaseio.com",
+    projectId: "clone-7ee5c",
+    storageBucket: "clone-7ee5c.appspot.com",
+    messagingSenderId: "785099066575",
+    appId: "1:785099066575:web:fd69c75eed327d93c2bbba"
+};
