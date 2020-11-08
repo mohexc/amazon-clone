978824a16ea6190ba433f33026cb394f995461f3
nanut <galunyu@gmail.com>
2020-11-08 12:58:28 +0700

    [Added] AppHeader Components

---
+++ src/layouts/AppHeader.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './AppHeader.less'
+import { BsSearch } from "react-icons/bs";
+
+
+const AppHeader = () => {
+    return (
+        <div className="app-header">
+            <img src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" className="app-header-logo" />
+            <div className="app-header-search">
+                <input type="text" className="app-header-searchInput" />
+                <BsSearch className="app-header-searchIcon" />
+            </div>
+            <div className="app-header-nav">
+                <div className="app-header-option">
+                    <span className="app-header-optionLineOne">Hello Guest</span>
+                    <span className="app-header-optionLineTwo">Sign In</span>
+                </div>
+                <div className="app-header-option">
+                    <span className="app-header-optionLineOne">Returns</span>
+                    <span className="app-header-optionLineTwo">& Orders</span>
+                </div>
+                <div className="app-header-option">
+                    <span className="app-header-optionLineOne">Your</span>
+                    <spab className="app-header-optionLineTwo">Prime</spab>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default AppHeader
